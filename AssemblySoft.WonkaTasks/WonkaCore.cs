a2daf2321ec2030ea58c0443670e3b4a76ac4b2e
lv31487 <carl.randall@lv.com>
2018-09-07 10:42:45 +0100

    Add project files.

---
+++ AssemblySoft.WonkaTasks/WonkaCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblySoft.WonkaTasks
+{
+    public class WonkaCore
+    {
+    }
+}
